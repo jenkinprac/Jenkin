4e034970530bd993fdacc5ca1e0a02eabd577c1a
Naveenkumar Doddipalli <Naveenkumar.Doddipalli@fadv.com>
2019-03-27 11:15:39 +0530

    changes done

--- Jenkins.java
+++
@@ -1,9 +0,0 @@
-public class Jenkins {
-	
-	public static void main(String[] args) {
-		System.out.println("This is to test jenkins");
-			System.out.println("This is to test jenkins by Naveen");
-		
-	}
-
-}
